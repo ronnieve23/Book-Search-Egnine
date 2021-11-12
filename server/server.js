a8fc04bf2168c864c77c94668df3aaad70c4e38c
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-11-12 15:46:21 -0500

    server changes for bug fix

--- server/server.js
+++ server/server.js
@@ -1,35 +1,44 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
+const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
+const path = require('path');
+// const routes = require('./routes');
 
-const {ApolloServer} = require('apollo-server-express');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
-const {typeDefs, resolvers}= require('./schemas');
-
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context:authMiddleware
-});
-
-server.applyMiddleware({app});
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware, 
   });
-});
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });
+};
+
+
+
+startServer();
