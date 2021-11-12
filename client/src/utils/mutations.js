1325961f46a709fe61b4d02b60872903d05a78eb
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-11-12 10:35:21 -0500

    added apollo provider to front end but forms are not working

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,27 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!){
+        loginUser(email: $email, password: $password){
+            token
+            user{
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $password: String!){
+        addUser(username: $username, email: $email, password: $password){
+            token
+            user{
+                _id
+                username
+            }
+        }
+    }
+`;
+
+
